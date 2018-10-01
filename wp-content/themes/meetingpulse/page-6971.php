a98cf8a4c2f9c5b1e77c993a572686f5ee4789b9
vadym <vadiknazarenko@gmail.com>
2018-10-01 15:49:37 +0300

    - add try for free btn for yearly

--- wp-content/themes/meetingpulse/page-6971.php
+++ wp-content/themes/meetingpulse/page-6971.php
@@ -430,7 +430,7 @@
                                     <div class="table__cell"><span class="table__show table__show_from"></span><span>$0 </span><span class="table__show">/ mo</span></div>
                                     <div class="table__cell"><span class="table__show table__show_from"></span><span>$75 </span><span class="table__show">/ mo</span></div>
                                     <div class="table__cell"><span class="table__show table__show_from">From </span><span>$315 </span><span class="table__show">/ mo</span></div>
-                                    <div class="table__cell"><span class="table__show">From </span><span>$625 </span><span class="table__show">/ mo</span></div>
+                                    <div class="table__cell"><a href="https://app.meet.ps/login/signup/?referer=wp-enterprise" class="try-for-free-purple" target="_blank">Try for free</a></div>
                                 </div>
                                 <div class="table__row table__row_price js-tabs-item" style="display: none;">
                                     <div class="table__cell"><span class="table__info">One-time cost</span></div>
