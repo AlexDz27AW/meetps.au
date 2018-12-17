4fc5be95644568b2195f1ef010e323fb4f00871c
Your Name <vadiknazarenko@gmail.com>
2018-12-17 19:43:01 +0200

    - footer scripts async

--- wp-content/themes/meetingpulse/footer.php
+++ wp-content/themes/meetingpulse/footer.php
@@ -194,7 +194,7 @@
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLT3HV5"
                           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
-        <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ubl9mgf7';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+        <script async="async">(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ubl9mgf7';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
         <script async="async" src="/wp-content/themes/meetingpulse/js/tooltipster.bundle.min.js"></script>
         <script async="async" src="/wp-content/themes/meetingpulse/js/jquery.backstretch.min.js"></script>
         <script async="async" src="/wp-content/themes/meetingpulse/js/owl.carousel.min.js"></script>
