30ff44a6c42a2763cd3d1471aa294dd13eb738ae
Your Name <vadiknazarenko@gmail.com>
2018-12-17 17:54:57 +0200

    - footer scripts async

--- wp-content/themes/meetingpulse/footer.php
+++ wp-content/themes/meetingpulse/footer.php
@@ -122,7 +122,7 @@
                 </div>
             </div>
             <!-- Calendly inline widget begin-->
-            <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
+            <script type="text/javascript" async="async" src="https://assets.calendly.com/assets/external/widget.js"></script>
             <!-- Calendly inline widget end-->
         </footer>
         </div>
@@ -195,11 +195,11 @@
                           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
         <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ubl9mgf7';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
-        <script src="/wp-content/themes/meetingpulse/js/tooltipster.bundle.min.js"></script>
-        <script src="/wp-content/themes/meetingpulse/js/jquery.backstretch.min.js"></script>
-        <script src="/wp-content/themes/meetingpulse/js/owl.carousel.min.js"></script>
-        <script src="/wp-content/themes/meetingpulse/js/tweenMax.min.js"></script>
-        <script src="/wp-content/themes/meetingpulse/js/smooth-scroll.min.js"></script>
+        <script async="async" src="/wp-content/themes/meetingpulse/js/tooltipster.bundle.min.js"></script>
+        <script async="async" src="/wp-content/themes/meetingpulse/js/jquery.backstretch.min.js"></script>
+        <script async="async" src="/wp-content/themes/meetingpulse/js/owl.carousel.min.js"></script>
+        <script async="async" src="/wp-content/themes/meetingpulse/js/tweenMax.min.js"></script>
+        <script async="async" src="/wp-content/themes/meetingpulse/js/smooth-scroll.min.js"></script>
 
     <?php wp_footer(); ?>
 
